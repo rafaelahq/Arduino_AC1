f8456c99b7e56c078db1029dca38757dec5d4926
Gabriel Gurgel Vicente Correia Alves <81368278+gabrielgvca0106@users.noreply.github.com>
2021-03-31 14:28:01 -0300

    Update AC1.ino

--- AC1.ino
+++ AC1.ino
@@ -1,8 +1,8 @@
-const int vermelho = 5;
+econst int vermelho = 5;
 const int verde = 6;
 const int azul = 7;
 
-bool estadoLedVermelho = false;
+boolean estadoLedVermelho = false;
 
 const int botao1 = 2;
 unsigned long lastDebounceTime1 = 0;
