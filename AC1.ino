7431e485247a38fbdaeb9504bf4597fa1f6de086
Gabriel Gurgel Vicente Correia Alves <81368278+gabrielgvca0106@users.noreply.github.com>
2021-04-01 13:14:53 -0300

    Update AC1.ino

--- AC1.ino
+++ AC1.ino
@@ -1,65 +1,96 @@
+//Inicialização das constantes do led
 const int vermelho = 5;
 const int verde = 6;
 const int azul = 7;
 
-boolean estadoLedVermelho = false;
+bool estadoLedVermelho = false;
 
 const int botao1 = 2;
+const int botao2 = 3;
+
 unsigned long lastDebounceTime1 = 0;
+unsigned long lastDebounceTime2 = 0;
 const int botaoDelay = 100;
 
+//Incicializacção do Setup
 void setup()
 {
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
-  
+
   pinMode(vermelho, OUTPUT);
   pinMode(verde, OUTPUT);
-  pinMode(azul, saida);
-  
+  pinMode(azul, OUTPUT);
+
   Serial.begin(9600);
-	
+
   Serial.println("AC1 - Meu Primeiro Projeto 2021");
   Serial.println("                           V1.0");
-  Serial.println("Grupo: NOME AQUI               ");
+  Serial.println("Grupo: ----> QUINTETO FANTÁSTICO  <-------");
 }
+
+//DETERMINANDO O LOOP
 
 void loop()
 {
+  // REGRA DECISÃO DO BOTÃO 1
   if((millis() - lastDebounceTime1) > botaoDelay && digitalRead(botao1)){
-  	Serial.println("botao 1 apertado");
-    ledVermelho();
-  	lastDebounceTime1 = millis();
+      Serial.println("Produção iniciada");
+    ledVermelho(true);
+      lastDebounceTime1 = millis();
+ }
+
+ //REGRA DECISÃO DO BOTÃO 2
+  if((millis() - lastDebounceTime2) > botaoDelay && digitalRead(botao2)){
+      Serial.println("Producão parada");
+    ledVermelho(false);
+      lastDebounceTime2 = millis();
   }
-  
-  if(getTemperatura() > 30){
+	
+//REGRA DE DECISÃO DO LED AZUL
+  if(getTemperatura() > 15){
     ledAzul(true);
+    Serial.println("Temperatura acima do correto");
   }else{
-  	ledAzul(falso); 
+    ledAzul(false);
+    Serial.println("Temperatura correta");
   }
-  	
+	
+//REGRA DE DECISÃO DO LED VERDE
+  if(getLuminosidade() > 5){
+    ledVerde(true);
+    Serial.println("Luminosidade acima do correto");
+  }else{
+    ledVerde(false);
+    Serial.println("Luminosidade correta");
+  }
+
   delay(10);
 }
 
-void ledVermelho(){
-  estadoLedVermelho = !estadoLedVermelho;
-  digitalWrite(vermelho,estadoLedVermelho);
+//DEFININDO ESTADOS DO LED
+void ledVermelho(bool estado){
+  digitalWrite(vermelho,estado);
 }
-void ledVerde(){
- //precisa fazer  
+void ledVerde(bool estado){
+ digitalWrite(verde,estado);
 }
 void ledAzul(bool estado){
-	digitalWrite(azul,estado);
+    digitalWrite(azul,estado);
 }
 
+// CÁLCULO DA TEMPERATURA EM GRAUS CÉLCIUS
 int getTemperatura(){
-  	int temperaturaC;
-	temperaturaC = map(((analogRead(A0) - 20) * 3.04), 0, 1023, -40, 125);
-  	return temperaturaC;
+      int temperaturaC; 
+    temperaturaC = map(((analogRead(A0) - 20) * 3.04), 0, 1023, -40, 125);
+      return temperaturaC;
 } 
 
+// CÁLCULO DA LUMINOSIDADE
+int getTemperatura(){
 int getLuminosidade(){
-  	int luminosidade;
-	luminosidade = map(analogRead(A1), 6, 619, -3, 10);
-  	return luminosidade;
+      int luminosidade;
+    luminosidade = map(analogRead(A1), 6, 619, -3, 10);
+      return luminosidade;
 }
+
